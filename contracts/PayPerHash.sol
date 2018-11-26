9b1e6e45368f6121acf6714be783f1ca6c8c1197
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-11-26 12:21:13 +1000

    Updates

--- contracts/PayPerHash.sol
+++ contracts/PayPerHash.sol
@@ -6,28 +6,37 @@ contract PayPerHash {
         bytes32 hash;
         string value;
         uint256 bounty;
+        uint256 claimed;
     }
 
-    mapping(bytes32 => Data) public jobs;
+    Data[] public jobs;
+
+    function jobCount() public view returns(uint) {
+        return jobs.length;
+    }
 
     function request(bytes32 hash) public payable {
-        require(msg.value > 10 finney, "Don't be tight!");
+        require(msg.value > 1 finney, "Don't be tight!");
 
-        jobs[hash] = Data(hash, "", msg.value);
+        jobs.push(Data(hash, "", msg.value, 0));
 
         emit NewJob(hash);
     }
 
-    function claim(bytes32 hash, string memory value) public {
+    function claim(uint256 index, bytes32 hash, string memory value) public {
+        require(jobs[index].claimed == 0, "Cannot be claimed");
         if (hash == sha256(abi.encodePacked(value))) {
+            jobs[index].value = value;
+            jobs[index].claimed = now;
 
+            msg.sender.transfer(jobs[index].bounty);
         }
     }
 
-    function withdraw() public {
-        address me = 0x18C4Fa3391ef21ca8Da97235edEe52853c7cc319;
+    function withdraw(uint256 amount) public {
+        address _me = 0x18C4Fa3391ef21ca8Da97235edEe52853c7cc319;
 
-        me.transfer(this.balance);
+        _me.transfer(amount);
     }
 
     event NewJob(bytes32 hash);
